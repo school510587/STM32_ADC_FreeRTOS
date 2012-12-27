baf67c5e301a6f6473d0219af1ad7a43075a0668
akasha00 <akashazero@gmail.com>
2012-12-27 20:59:39 +0800

    simple GPIO demo with RTOS

--- main.c
+++ main.c
@@ -24,10 +24,21 @@ __IO uint8_t UserButtonPressed = 0x00;
 
 
 /* Private function prototypes -----------------------------------------------*/
-static void TIM4_Config(void);
-static void Demo_Exec(void);
+static void LED_task(void *pvParameters);
+static void button_task(void *pvParameters);
 
-/* Private functions ---------------------------------------------------------*/
+/**
+  * @brief  This function handles EXTI0_IRQ Handler.
+  * @param  None
+  * @retval None
+  */
+void EXTI0_IRQHandler(void)
+{
+  UserButtonPressed = 0x01;
+  
+  /* Clear the EXTI line pending bit */
+  EXTI_ClearITPendingBit(USER_BUTTON_EXTI_LINE);
+}
 
 int main(void)
 {
@@ -35,245 +46,76 @@ int main(void)
   
   /* Initialize LEDs and User_Button on STM32F4-Discovery --------------------*/
   STM_EVAL_PBInit(BUTTON_USER, BUTTON_MODE_EXTI); 
-  
+
+  /* Initialize LEDs to be managed by GPIO */
   STM_EVAL_LEDInit(LED4);
   STM_EVAL_LEDInit(LED3);
   STM_EVAL_LEDInit(LED5);
   STM_EVAL_LEDInit(LED6);
-  
-  /* SysTick end of count event each 10ms */
-  RCC_GetClocksFreq(&RCC_Clocks);
-  SysTick_Config(RCC_Clocks.HCLK_Frequency / 100);
-  
 
-  Demo_Exec();
+  /* Turn OFF all LEDs */
+  STM_EVAL_LEDOff(LED4);
+  STM_EVAL_LEDOff(LED3);
+  STM_EVAL_LEDOff(LED5);
+  STM_EVAL_LEDOff(LED6);
+    
+  /* Reset UserButton_Pressed variable */
+  UserButtonPressed = 0x00;
 
+  /* Create a task to flash the LED. */
+  xTaskCreate(LED_task,
+             (signed portCHAR *) "LED Flash",
+             512 /* stack size */, NULL,
+             tskIDLE_PRIORITY + 5, NULL);
+
+  /* Create a task to button check. */
+  xTaskCreate(button_task,
+             (signed portCHAR *) "User Button",
+             512 /* stack size */, NULL,
+             tskIDLE_PRIORITY + 5, NULL);
+
+  /* Start running the tasks. */
+  vTaskStartScheduler(); 
+
+  return 0;
 }
 
-/**
-  * @brief  Execute the demo application.
-  * @param  None
-  * @retval None
-  */
-static void Demo_Exec(void)
+static void LED_task(void *pvParameters)
 {
   RCC_ClocksTypeDef RCC_Clocks;
   uint8_t togglecounter = 0x00;
-  
+
   while(1)
-  {
-    
-    /* Reset UserButton_Pressed variable */
-    UserButtonPressed = 0x00;
-    
-    /* Initialize LEDs to be managed by GPIO */
-    STM_EVAL_LEDInit(LED4);
-    STM_EVAL_LEDInit(LED3);
-    STM_EVAL_LEDInit(LED5);
-    STM_EVAL_LEDInit(LED6);
-    
-    /* SysTick end of count event each 10ms */
-    RCC_GetClocksFreq(&RCC_Clocks);
-    SysTick_Config(RCC_Clocks.HCLK_Frequency / 100);  
-    
-    /* Turn OFF all LEDs */
-    STM_EVAL_LEDOff(LED4);
-    STM_EVAL_LEDOff(LED3);
-    STM_EVAL_LEDOff(LED5);
-    STM_EVAL_LEDOff(LED6);
-    
-    /* Waiting User Button is pressed */
-    while (UserButtonPressed == 0x00)
-    {
-      /* Toggle LED4 */
-      STM_EVAL_LEDToggle(LED4);
-      Delay(10);
-      /* Toggle LED4 */
-      STM_EVAL_LEDToggle(LED3);
-      Delay(10);
-      /* Toggle LED4 */
+  {    
+      /* Toggle LED5 */
       STM_EVAL_LEDToggle(LED5);
-      Delay(10);
-      /* Toggle LED4 */
+      vTaskDelay(100);
+      /* Toggle LED6 */
       STM_EVAL_LEDToggle(LED6);
-      Delay(10);
-      togglecounter ++;
-      if (togglecounter == 0x10)
-      {
-        togglecounter = 0x00;
-        while (togglecounter < 0x10)
-        {
-          STM_EVAL_LEDToggle(LED4);
-          STM_EVAL_LEDToggle(LED3);
-          STM_EVAL_LEDToggle(LED5);
-          STM_EVAL_LEDToggle(LED6);
-          Delay(10);
-          togglecounter ++;
-        }
-       togglecounter = 0x00;
-      }
-    }
-    
-    /* Waiting User Button is Released */
-    while (STM_EVAL_PBGetState(BUTTON_USER) == Bit_SET)
-    {}
-    UserButtonPressed = 0x00;
-    
-    /* TIM4 channels configuration */
-    TIM4_Config();
-    
-    /* Disable all Timer4 channels */
-    TIM_CCxCmd(TIM4, TIM_Channel_1, DISABLE);
-    TIM_CCxCmd(TIM4, TIM_Channel_2, DISABLE);
-    TIM_CCxCmd(TIM4, TIM_Channel_3, DISABLE);
-    TIM_CCxCmd(TIM4, TIM_Channel_4, DISABLE);
-    
-    /* Waiting User Button is pressed */
-    while (UserButtonPressed == 0x00)
-    {}
-    
-    /* Waiting User Button is Released */
-    while (STM_EVAL_PBGetState(BUTTON_USER) == Bit_SET)
-    {}
+      vTaskDelay(100);
   }
 }
 
-/**
-  * @brief  Configures the TIM Peripheral.
-  * @param  None
-  * @retval None
-  */
-static void TIM4_Config(void)
+static void button_task(void *pvParameters)
 {
-  GPIO_InitTypeDef GPIO_InitStructure;
-  TIM_OCInitTypeDef  TIM_OCInitStructure;
-  TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
-  
-  /* --------------------------- System Clocks Configuration -----------------*/
-  /* TIM4 clock enable */
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
-  
-  /* GPIOD clock enable */
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-
-  /*-------------------------- GPIO Configuration ----------------------------*/
-  /* GPIOD Configuration: Pins 12, 13, 14 and 15 in output push-pull */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_Init(GPIOD, &GPIO_InitStructure);
-
-  /* Connect TIM4 pins to AF2 */  
-  GPIO_PinAFConfig(GPIOD, GPIO_PinSource12, GPIO_AF_TIM4);
-  GPIO_PinAFConfig(GPIOD, GPIO_PinSource13, GPIO_AF_TIM4); 
-  GPIO_PinAFConfig(GPIOD, GPIO_PinSource14, GPIO_AF_TIM4);
-  GPIO_PinAFConfig(GPIOD, GPIO_PinSource15, GPIO_AF_TIM4); 
-  
-    /* -----------------------------------------------------------------------
-    TIM4 Configuration: Output Compare Timing Mode:
-    
-    In this example TIM4 input clock (TIM4CLK) is set to 2 * APB1 clock (PCLK1), 
-    since APB1 prescaler is different from 1 (APB1 Prescaler = 4, see system_stm32f4xx.c file).
-      TIM4CLK = 2 * PCLK1  
-      PCLK1 = HCLK / 4 
-      => TIM4CLK = 2*(HCLK / 4) = HCLK/2 = SystemCoreClock/2
-         
-    To get TIM4 counter clock at 2 KHz, the prescaler is computed as follows:
-       Prescaler = (TIM4CLK / TIM1 counter clock) - 1
-       Prescaler = (168 MHz/(2 * 2 KHz)) - 1 = 41999
-                                        
-    To get TIM4 output clock at 1 Hz, the period (ARR)) is computed as follows:
-       ARR = (TIM4 counter clock / TIM4 output clock) - 1
-           = 1999
-                    
-    TIM4 Channel1 duty cycle = (TIM4_CCR1/ TIM4_ARR)* 100 = 50%
-    TIM4 Channel2 duty cycle = (TIM4_CCR2/ TIM4_ARR)* 100 = 50%
-    TIM4 Channel3 duty cycle = (TIM4_CCR3/ TIM4_ARR)* 100 = 50%
-    TIM4 Channel4 duty cycle = (TIM4_CCR4/ TIM4_ARR)* 100 = 50%
-    
-    ==> TIM4_CCRx = TIM4_ARR/2 = 1000  (where x = 1, 2, 3 and 4).
-  
-    Note: 
-     SystemCoreClock variable holds HCLK frequency and is defined in system_stm32f4xx.c file.
-     Each time the core clock (HCLK) changes, user had to call SystemCoreClockUpdate()
-     function to update SystemCoreClock variable value. Otherwise, any configuration
-     based on this variable will be incorrect.    
-  ----------------------------------------------------------------------- */ 
-  
-  
-  /* Compute the prescaler value */
-  PrescalerValue = (uint16_t) ((SystemCoreClock /2) / 2000) - 1;
-  
-  /* Time base configuration */
-  TIM_TimeBaseStructure.TIM_Period = TIM_ARR;
-  TIM_TimeBaseStructure.TIM_Prescaler = PrescalerValue;
-  TIM_TimeBaseStructure.TIM_ClockDivision = 0;
-  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
-  TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
-  
-  /* Enable TIM4 Preload register on ARR */
-  TIM_ARRPreloadConfig(TIM4, ENABLE);
-  
-  /* TIM PWM1 Mode configuration: Channel */
-  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
-  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-  TIM_OCInitStructure.TIM_Pulse = TIM_CCR;
-  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-  
-  /* Output Compare PWM1 Mode configuration: Channel1 */
-  TIM_OC1Init(TIM4, &TIM_OCInitStructure);
-  TIM_CCxCmd(TIM4, TIM_Channel_1, DISABLE);
-  
-  TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
-  
-  /* Output Compare PWM1 Mode configuration: Channel2 */
-  TIM_OC2Init(TIM4, &TIM_OCInitStructure);
-  TIM_CCxCmd(TIM4, TIM_Channel_2, DISABLE);
-  
-  TIM_OC2PreloadConfig(TIM4, TIM_OCPreload_Enable);
-    
-  /* Output Compare PWM1 Mode configuration: Channel3 */
-  TIM_OC3Init(TIM4, &TIM_OCInitStructure);
-  TIM_CCxCmd(TIM4, TIM_Channel_3, DISABLE);
-  
-  TIM_OC3PreloadConfig(TIM4, TIM_OCPreload_Enable);
-  
-  /* Output Compare PWM1 Mode configuration: Channel4 */
-  TIM_OC4Init(TIM4, &TIM_OCInitStructure);
-  TIM_CCxCmd(TIM4, TIM_Channel_4, DISABLE);
-  
-  TIM_OC4PreloadConfig(TIM4, TIM_OCPreload_Enable);
-  
-  /* TIM4 enable counter */
-  TIM_Cmd(TIM4, ENABLE);
+	while (1)
+	{
+		    /* Waiting User Button is pressed */
+    		    if (UserButtonPressed == 0x01)
+    		    {
+      		    	/* Toggle LED4 */
+      			STM_EVAL_LEDToggle(LED4);
+      			vTaskDelay(100);
+      			/* Toggle LED3 */
+      			STM_EVAL_LEDToggle(LED3);
+      			vTaskDelay(100);
+    		    }
+		    /* Waiting User Button is Released */
+    		    while (STM_EVAL_PBGetState(BUTTON_USER) == Bit_SET);
+		    UserButtonPressed = 0x00;
+	}
 }
 
-/**
-  * @brief  Inserts a delay time.
-  * @param  nTime: specifies the delay time length, in 10 ms.
-  * @retval None
-  */
-void Delay(__IO uint32_t nTime)
-{
-  TimingDelay = nTime;
-
-  while(TimingDelay != 0);
-}
-
-/**
-  * @brief  Decrements the TimingDelay variable.
-  * @param  None
-  * @retval None
-  */
-void TimingDelay_Decrement(void)
-{
-  if (TimingDelay != 0x00)
-  { 
-    TimingDelay--;
-  }
-}
 
 /**
   * @brief  This function handles the test program fail.
@@ -291,7 +133,7 @@ void Fail_Handler(void)
   {
     /* Toggle Red LED */
     STM_EVAL_LEDToggle(LED5);
-    Delay(5);
+    vTaskDelay(5);
   }
 }
 
